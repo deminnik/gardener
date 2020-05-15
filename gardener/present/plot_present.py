e0efd9c60d2fe663f29ac6143a7a2eaaa7c2110e
Nik <64067860+deminnik@users.noreply.github.com>
2020-05-15 14:36:51 +0700

    Window for displaying statistics

---
+++ gardener/present/plot_present.py
@@ -0,0 +1,3 @@
+class PlotPresenter:
+    def __init__(self, view):
+        self.view = view
--- gardener/view/forms.py
+++ gardener/view/forms.py
@@ -2,12 +2,17 @@ from os import path
 from functools import partial
 
 from qgis.PyQt import uic
-from qgis.PyQt.QtWidgets import QWidget, QFileDialog
+from qgis.PyQt.QtWidgets import QWidget, QFileDialog, QVBoxLayout
 from qgis.core import Qgis
 from qgis.core import QgsProject
 
+from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt4agg import NavigationToolbar2QT as NavigationToolbar
+from matplotlib.figure import Figure
+
 from gardener.present.main_present import MainPresenter
 from gardener.present.params_present import ParamsPresenter
+from gardener.present.plot_present import PlotPresenter
 
 
 class Form(QWidget):
@@ -34,6 +39,7 @@ class MainForm(Form):
         self.indexLayerComboBox.currentIndexChanged.connect(self.indexLayerChoose)
         self.paramsButton.clicked.connect(self.openParamsWidget)
         self.suppressButton.clicked.connect(self.unveilImage)
+        self.showButton.clicked.connect(self.openPlotWidget)
 
     def imageryLayerChoose(self):
         self.presenter.imagery_layer_choose(self.imageLayerComboBox.currentLayer())
@@ -52,6 +58,9 @@ class MainForm(Form):
 
     def openParamsWidget(self):
         self.manager.params_widget.show()
+
+    def openPlotWidget(self):
+        self.manager.plot_widget.show()
 
 
 class ParamsForm(Form):
@@ -98,3 +107,40 @@ class ParamsForm(Form):
     def showEvent(self, e):
         self.presenter.init_window(self.manager.parameters)
         QWidget.showEvent(self, e)
+
+
+class PlotForm(Form):
+
+    class PlotFrame(QWidget):
+        def __init__(self, curve, parent=None):
+            super().__init__(parent)
+            self.__figure = Figure()
+            self.__canvas = FigureCanvas(self.__figure)
+            self.__toolbar = NavigationToolbar(self.__canvas, self)
+            self.__set_layout()
+            self.__set_figure()
+            self.__plot_curve(curve)
+            self.__canvas.draw()
+
+        def __set_layout(self):
+            vbox = QVBoxLayout()
+            vbox.addWidget(self.__canvas)
+            vbox.addWidget(self.__toolbar)
+            self.setLayout(vbox)
+
+        def __set_figure(self):
+            self.__axes = self.__figure.add_subplot(111)
+            self.__axes.clear()
+
+        def __plot_curve(self, curve):
+            self.__axes.plot(curve[0], curve[1], linewidth=1, color='k')
+            
+
+    def __init__(self, manager, parent=None):
+        super().__init__("plot.ui", PlotPresenter, manager, parent)
+        frame = PlotForm.PlotFrame([[1,2,3,4,5,6,7,8,9], [1,2,3,4,5,6,7,8,9]])
+        self.addTab(frame, "Example")
+
+    def addTab(self, content, title):
+        self.tabWidget.addTab(content, title)
+        self.tabWidget.setCurrentIndex(self.tabWidget.count()-1)
